cc69b218cd260785549f26528bc947a8e07f078b
Jim Luo <jimluo2000@gmail.com>
2018-03-20 00:35:22 -0700

    Finished unit 12

--- Unit 12/src/Monster.java
+++ Unit 12/src/Monster.java
@@ -13,55 +13,97 @@ public class Monster implements Comparable
 	private int myAge;
 
 	//write a default Constructor
-
+	public Monster() {
+		setAge(0);
+		setHeight(0);
+		setWeight(0);
+	}
 
 
 	//write an initialization constructor with an int parameter ht
-
+	public Monster (int ht) {
+		setHeight(ht);
+	}
 
 
 	//write an initialization constructor with int parameters ht and wt
-
+	public Monster (int ht, int wt) {
+		setHeight(ht);
+		setWeight(wt);
+	}
 
 
 
 	//write an initialization constructor with int parameters ht, wt, and age
-
+	public Monster (int ht, int wt, int age) {
+		setHeight(ht);
+		setWeight(wt);
+		setAge(age);
+	}
 
 
 	//modifiers - write set methods for height, weight, and age
-
+	public void setAge(int age) {
+		myAge = age;
+	}
+	public void setHeight(int height) {
+		myHeight = height;
+	}
+	public void setWeight (int weight) {
+		myWeight = weight;
+	}
 	
 	
 	//accessors - write get methods for height, weight, and age
-	
+	public int getAge() {
+		return myAge;
+	}
+	public int getHeight() {
+		return myHeight;
+	}
+	public int getWeight() {
+		return myWeight;
+	}
 	
 	
 	//creates a new copy of this Object
 	public Object clone()
 	{
-	   return new Monster();
+	   return new Monster(myHeight, myWeight, myAge);
 	}
 
 	public boolean equals( Object obj )
 	{
-
+		Monster other = (Monster)obj;
+		if (other.getAge() == myAge
+				&&other.getHeight() == myHeight
+				&& other.getWeight() == myWeight) {
+			return true;
+		}
+			
 
 		return false;
 	}
 
 	public int compareTo( Object obj )
 	{
+		
 		Monster rhs = (Monster)obj;
-
-
-
+		
+		if (equals(rhs)) return 0;
+		
+		if (myHeight > rhs.getHeight()) return 1;
+		else if (myWeight > rhs.getWeight()) return 1;
+		else if (myAge > rhs.getAge()) return 1;
 
 
 		return -1;
 	}
 
 	//write a toString() method
-	
+	public String toString() {
+		String output = Integer.toString(myHeight) + " " + myWeight + " "+ myAge;
+		return output;
+	}
 	
 }
---
+++ Unit 12/src/d18/Lab18d.java
@@ -0,0 +1,56 @@
+package d18;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import static java.lang.System.*;
+
+public class Lab18d
+{
+	public static void main( String args[] ) throws IOException
+	{
+		Scanner file = new Scanner(new File("C:\\Users\\jimlu\\Documents\\GitHub\\APCompSciMauro\\Unit 12\\src\\d18\\lab18d.dat"));
+
+		int size = file.nextInt();
+		file.nextLine();
+		
+		Word w = new Word(file.nextLine());
+		ArrayList<Word> words = new ArrayList();
+		words.add(w);
+		
+		while(file.hasNextLine()) {
+			w = new Word(file.nextLine());
+			for(int i = 0; i<words.size(); i++) {
+				if (w.compareTo(words.get(i)) <=0) {
+					words.add(i, w);
+					break;
+				}
+				
+				}
+			if(! words.contains(w))words.add(w);
+			
+		}
+		System.out.println(words);
+		for(Word x : words) {
+			System.out.println(x.toString());
+		}
+
+
+
+
+
+
+
+
+
+
+	}
+}
