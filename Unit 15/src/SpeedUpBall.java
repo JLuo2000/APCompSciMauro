33cbe54aa6daf8ee28776b6d105e9051c0279e02
Jim Luo <jimluo2000@gmail.com>
2018-04-05 22:45:49 -0700

    Finished pong

--- Unit 15/src/SpeedUpBall.java
+++ Unit 15/src/SpeedUpBall.java
@@ -11,37 +11,48 @@ import java.util.Random;
 class SpeedUpBall extends Ball
 {
 
-   //instance variables
+   private int xSpeed;
+   private int ySpeed;
 
    public SpeedUpBall()
    {
-
+	   super(200,200);
+		xSpeed = 3;
+		ySpeed = 1;
 
    }
 
    public SpeedUpBall(int x, int y)
    {
-
+	   super(x,y);
+		xSpeed = 3;
+		ySpeed = 1;
 
    }
 
 
    public SpeedUpBall(int x, int y, int xSpd, int ySpd)
    {
-
+	   super(x,y);
+		xSpeed = xSpd;
+		ySpeed = ySpd;
 
    }
 
    public SpeedUpBall(int x, int y, int wid, int ht, int xSpd, int ySpd)
    {
-
+	   super(x,y, wid, ht);
+	   xSpeed = xSpd;
+	   ySpeed = ySpd;
 
    }
 
 
    public SpeedUpBall(int x, int y, int wid, int ht, Color col, int xSpd, int ySpd)
    {
-
+	   super(x,y, wid, ht, col);
+	   xSpeed = xSpd;
+	   ySpeed = ySpd;
 
 
 
@@ -49,7 +60,7 @@ class SpeedUpBall extends Ball
 
    public void setXSpeed( int xSpd )
    {
-
+	   xSpeed = xSpd;
 
 
 
@@ -58,7 +69,7 @@ class SpeedUpBall extends Ball
    public void setYSpeed( int ySpd )
    {
 
-
+	   ySpeed = ySpd;
 
 
    }
