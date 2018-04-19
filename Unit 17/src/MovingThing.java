131b79159901ac6f7c1a04612f9d150dc87831dd
Jim Luo <jimluo2000@gmail.com>
2018-04-18 21:38:24 -0700

    Unit 17

---
+++ Unit 17/src/MovingThing.java
@@ -0,0 +1,73 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public abstract class MovingThing implements Locatable
+{
+	private int xPos;
+	private int yPos;
+
+	public MovingThing()
+	{
+		xPos = 0;
+		yPos = 0;
+	}
+
+	public MovingThing(int x, int y)
+	{
+		setPos(x,y);
+	}
+
+	public void setPos( int x, int y)
+	{
+		setX(x);
+		setY(y);
+	}
+
+
+	public void setX(int x)
+	{
+		xPos = x;
+	}
+
+
+	public void setY(int y)
+	{
+		yPos = y;
+	}
+
+	public int getX()
+	{
+		return xPos;
+	}
+
+
+	public int getY()
+	{
+		return yPos;
+	}
+
+	public abstract void setSpeed( int s );
+	public abstract int getSpeed();
+	public abstract void draw(Graphics window);
+
+	public void move(String direction)
+	{
+		if(direction.equals("LEFT"))
+	      setX(getX()-getSpeed());
+
+      	if(direction.equals("RIGHT")) {
+      		setX(getX() + getSpeed());
+      	}
+	}
+
+	public String toString()
+	{
+		return "";
+	}
+}
